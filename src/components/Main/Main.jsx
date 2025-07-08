e9e5c9baea1bec09f467b84a2737ffc871f61f91
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2025-07-08 23:26:23 +0530

    #bug7-fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
 import { Context } from '../../context/Context';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const Main = () => {
 
@@ -21,7 +23,7 @@ const Main = () => {
             ? 
             <>
                 <div className="greet">
-                <p><span>Hello,</span></p>
+                <p><span>Hello, Aryan</span></p>
                 <p>How can I help you today?</p>
                 </div>
                 <div className="cards">
@@ -56,7 +58,11 @@ const Main = () => {
                         <hr />
                         <hr />
                     </div>
-                    :<p dangerouslySetInnerHTML={{ __html: resultData }}></p>}
+                    :<div className="markdown-output">
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                            {resultData}
+                        </ReactMarkdown>
+                    </div>}
                     
                 </div>
              </div>
@@ -65,7 +71,7 @@ const Main = () => {
 
             <div className="main-bottom">
                 <div className="search-box">
-                    <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Ask...'/>
+                    <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here'/>
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
